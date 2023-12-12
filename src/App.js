899219dec4fa66b2fd4c69115593a87c90843d07
Oleg Kashkanov <o.kashkanov@gmail.com>
2023-12-12 15:51:24 +0300

    color change green

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DIE
+          Learn DIEasdqwe
         </a>
       </header>
     </div>
